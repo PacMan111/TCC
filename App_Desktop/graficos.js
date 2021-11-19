d47539616d7640f5a81df77bb0bf3bfb4c08abb9
PacMan111 <luizgustavossj@gmail.com>
2021-11-19 17:20:01 -0400

    corrigido app desktop

--- App_Desktop/graficos.js
+++ App_Desktop/graficos.js
@@ -1,4 +1,4 @@
-const url = "https://script.google.com/macros/s/AKfycbypIIHYPQhdDOJTjZXxE47MCy3dn0ijYFnvy3Sfyi_Jaxv7tDerMwAdXLZ7pvH_0Qaw/exec?action=24HOURS";
+const url = "https://script.google.com/macros/s/AKfycbz3LNO6KmwctGSD-M6JvUuOSc8OkHXSK7ibzFJYBls9rmR7ayyJLYvwvbZZ3oJRxMUH/exec";
 const fetch = require('node-fetch');
 
 var Chart = require('chart.js');
@@ -13,11 +13,10 @@ var options = {
 
 let labels = []
 
-let arr_temperaturasDS = []
+let arr_temperaturas = []
 let arr_PH = []
 let arr_TDS = []
 let arr_Condutividade = []
-let arr_Turbidez = []
 
 var ctx = document.getElementById('graficoTemperatura').getContext('2d');
 var graficoTemperatura = new Chart(ctx, {
@@ -29,7 +28,7 @@ data: {
             label: "Temperatura da Água",
             backgroundColor: '#50F',
             borderColor: '#50F',
-            data: arr_temperaturasDS
+            data: arr_temperaturas
         }
     ]
 },
@@ -38,59 +37,42 @@ options: options
 
 var ctx = document.getElementById('graficoPH').getContext('2d');
 var graficoPH = new Chart(ctx, {
-type: 'line',
-data: {
-    labels: labels,
-    datasets: [
-        {
-            label: "PH",
-            backgroundColor: '#50F',
-            borderColor: '#50F',
-            data: arr_PH
-        }
-    ]
-},
-options: options
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: "PH",
+                backgroundColor: '#50F',
+                borderColor: '#50F',
+                data: arr_PH
+            }
+        ]
+    },
+    options: options
 });
 
 var ctx = document.getElementById('graficoCondutividade_TDS').getContext('2d');
 var graficoCondutividade_TDS = new Chart(ctx, {
-type: 'line',
-data: {
-    labels: labels,
-    datasets: [
-        {
-            label: "Total de Solidos Dissolvidos",
-            backgroundColor: '#AAF',
-            borderColor: '#AAF',
-            data: arr_TDS
-        },
-        {
-            label: "Condutividade Elétrica",
-            backgroundColor: '#50F',
-            borderColor: '#50F',
-            data: arr_Condutividade
-        }
-    ]
-},
-options: options
-});
-
-var ctx = document.getElementById('graficoTurbidez').getContext('2d');
-var graficoTurbidez = new Chart(ctx, {
-type: 'line',
-data: {
-    labels: labels,
-    datasets: [
-        {
-            label: "Turbidez",
-            backgroundColor: '#50F',
-            borderColor: '#50F',
-            data: arr_Turbidez
-        }
-    ]
-},
-options: options
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: "Total de Solidos Dissolvidos",
+                backgroundColor: '#AAF',
+                borderColor: '#AAF',
+                data: arr_TDS
+            },
+            {
+                label: "Condutividade Elétrica",
+                backgroundColor: '#50F',
+                borderColor: '#50F',
+                data: arr_Condutividade
+            }
+        ]
+    },
+    options: options
 });
 
 function getDados(){
@@ -106,66 +88,71 @@ function getDados(){
     })
     .then(responseJson => {
 
-    let valores = responseJson.valores
+        let valores24Horas = responseJson.valores24Horas
+        let ultimosValores = responseJson.ultimosValores
 
-    var ultimaAtualizacao = ''
+        console.log(valores24Horas)
+        console.log(ultimosValores)
 
-    valores.forEach(valor => {
-        let ano = valor[0].substring(0, valor[0].indexOf('-'))
-        let mes = valor[0].substring(valor[0].indexOf(ano) + 5, valor[0].indexOf(ano) + 7)
-        let dia = valor[0].substring(valor[0].indexOf(mes) + 3, valor[0].indexOf('T'))
-        let hora = valor[0].substring(valor[0].indexOf('T') + 1, valor[0].indexOf('.'))
+        valores24Horas.forEach(valor => {
+            if(valor[0].length > 0){
+                labels.push(converterDatas(valor[0]))
+                arr_temperaturas.push(valor[1])
+                arr_TDS.push(valor[2])
+                arr_Condutividade.push(parseInt(valor[2]) * 2 / 1000)
+                arr_PH.push(parseFloat(valor[3]) / 100)
+            }
+        });
 
-        let data
 
-        if(valor[0].length > 0){
-            data = dia + "/" + mes + '/' + ano + " " + hora
-            ultimaAtualizacao = data;
+        let dataUltimaAtt = converterDatas(ultimosValores.hora)
 
-            labels.push(data)
-            arr_TDS.push(valor[2])
-            arr_Condutividade.push(parseInt(valor[2]) * 2 / 1000)
-            arr_Turbidez.push(valor[3])
-            arr_PH.push(valor[4])
-            arr_temperaturasDS.push(valor[5])
-        }
-        
-    });
+        document.getElementById("tempAgua").innerHTML = ultimosValores.temperatura
+        document.getElementById("tds").innerHTML = ultimosValores.tds
+        document.getElementById("ph").innerHTML = parseFloat(ultimosValores.pH) / 100
+        document.getElementById("condEletrica").innerHTML = parseInt(ultimosValores.tds) * 2 /1000
+        document.getElementById('horario').innerHTML = `Última atualização: ${dataUltimaAtt}`
 
-    document.getElementById("tds").innerHTML = valores[valores.length-1][2]
-    document.getElementById("turbidez").innerHTML = valores[valores.length-1][3]
-    document.getElementById("ph").innerHTML = valores[valores.length-1][4]
-    document.getElementById("tempAgua").innerHTML = valores[valores.length-1][5]
-
-    document.getElementById("condEletrica").innerHTML = parseInt(valores[valores.length-1][2]) * 2 /1000
-
-    document.getElementById('horario').innerHTML = `Última atualização: ${ultimaAtualizacao}`
-
-    atualizarGraficos()
+        atualizarGraficos()
     });
 }
 
 function atualizarGraficos(){
-    graficoTemperatura.data.datasets[0].data = arr_temperaturasDS
+    graficoTemperatura.data.datasets[0].data = arr_temperaturas
     graficoPH.data.datasets[0].data = arr_PH
     graficoCondutividade_TDS.data.datasets[0].data = arr_TDS
     graficoCondutividade_TDS.data.datasets[1].data = arr_Condutividade
-    graficoTurbidez.data.datasets[0].data = arr_Turbidez
 
     graficoTemperatura.update()
     graficoPH.update()
     graficoCondutividade_TDS.update()
-    graficoTurbidez.update()
 }
 
-getDados();
+loop()
 
 setInterval(() => {
+    loop()
+}, 30000)
+
+function loop(){
+    getDados()
     labels.splice(0, labels.length)
     arr_TDS.splice(0, arr_TDS.length)
     arr_Condutividade.splice(0, arr_Condutividade.length)
-    arr_Turbidez.splice(0, arr_Turbidez.length)
     arr_PH.splice(0, arr_PH.length)
-    arr_temperaturasDS.splice(0, arr_temperaturasDS.length)
-    getDados();
-}, 30000)
+    arr_temperaturas.splice(0, arr_temperaturas.length)
+}
+
+function converterDatas(dataGoogle){
+    
+   if(dataGoogle != null && dataGoogle != undefined && dataGoogle != ""){
+    let ano = dataGoogle.substring(0, dataGoogle.indexOf('-'))
+    let mes = dataGoogle.substring(dataGoogle.indexOf(ano) + 5, dataGoogle.indexOf(ano) + 7)
+    let dia = dataGoogle.substring(dataGoogle.indexOf(mes) + 3, dataGoogle.indexOf('T'))
+    let hora = dataGoogle.substring(dataGoogle.indexOf('T') + 1, dataGoogle.indexOf('.'))
+
+    return dia + "/" + mes + '/' + ano + " " + hora
+   }
+
+   return ''
+}
